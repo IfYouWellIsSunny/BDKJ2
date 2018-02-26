b6e160e6817a3282a63469a7d6c9797259dcbc44
墨鸦 <1663997737@qq.com>
2018-02-26 08:45:46 +0800

    测试

--- app/src/main/java/com/example/dell/bdkj/MainActivity.java
+++ app/src/main/java/com/example/dell/bdkj/MainActivity.java
@@ -8,6 +8,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //隐藏标题栏
+        getSupportActionBar().hide();
+        //加载布局
         setContentView(R.layout.activity_main);
     }
 }
